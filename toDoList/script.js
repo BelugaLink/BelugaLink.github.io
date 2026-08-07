62320d9d86955ea10258195e1f9c5212fe1043fd
Nama Anda <linkkbeluga@gmail.com>
2026-08-07 19:53:03 +0700

    tambah project random

---
+++ toDoList/script.js
@@ -0,0 +1,32 @@
+{/* <input type="text" id="inputTask">
+<button id="submitBtn">submit</button> */}
+const doc = {
+    inputTask: document.getElementById("inputTask"),
+    submitBtn: document.getElementById("submitBtn"),
+    taskContainer: document.getElementById("taskContainer"),
+}
+let taskList = [
+    {tugas: "Tugas bindo"},
+    {tugas: "Tugas binggris"},
+    {tugas: "Tugas kk"}
+]
+function rendering () {
+    taskList.forEach((item, indeks) => {
+        doc.taskContainer.innerHTML += `
+        <p>${indeks + 1}. ${item.tugas}</p>
+        `
+    })
+}
+function submitTask(task) {
+    //membersihkan task container sebelum di render ulang
+    doc.taskContainer.innerHTML = ''
+    if (typeof task !== 'string' || task === '') {
+        doc.taskContainer.innerHTML = '<p>Masukkan nilai yang valid</p>'
+        return
+    }
+    let cleanValue = task.charAt(0).toUpperCase() + task.slice(1).toLowerCase();
+    cleanValue = cleanValue.replace(/\s+/g, ' ').trim()
+    taskList.push({tugas: cleanValue})
+    rendering()
+}
+doc.submitBtn.addEventListener('click', () => {submitTask(doc.inputTask.value)})
